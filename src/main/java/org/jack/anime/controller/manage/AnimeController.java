1cdbde5026443d481fc9095319f4d23fc8fc3533
suyi <suyi@syhvip.com>
2018-02-23 18:08:42 +0800

    优化

--- src/main/java/org/jack/anime/controller/manage/AnimeController.java
+++ src/main/java/org/jack/anime/controller/manage/AnimeController.java
@@ -4,6 +4,8 @@
 package org.jack.anime.controller.manage;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author jack
@@ -11,5 +13,11 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class AnimeController {
-
+	
+	// 登录跳转
+	@RequestMapping(value = "/anime/schedule/list", method = { RequestMethod.GET })
+	public String animeScheduleList() {
+		System.out.println("BBB");
+		return "admin/image";
+	}
 }
---
+++ src/main/java/org/jack/anime/service/vo/animeTimetable/Result.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package org.jack.anime.service.vo.animeTimetable;
+
+/**
+ * 
+ * 封装json对象，所有返回结果都使用它
+ * @author jack
+ *
+ */
+public class Result<T> {
+	
+	private boolean success;// 是否成功标志
+
+	private T data;// 成功时返回的数据
+
+	private String error;// 错误信息
+
+	
+	public Result() {
+	}
+
+	public Result(boolean success, T data) {
+		this.success = success;
+		this.data = data;
+	}
+
+	public Result(T data, String error) {
+		this.data = data;
+		this.error = error;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+	
+	
+}
