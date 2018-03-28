1d7d08e2afcbb4a7f05f8f0b656b3d424988ea1d
suyi <suyi@syhvip.com>
2018-03-28 18:05:33 +0800

    权限认证配置优化

--- src/main/java/org/jack/anime/realm/AuthRealm.java
+++ src/main/java/org/jack/anime/realm/AuthRealm.java
@@ -11,14 +11,24 @@ import org.springframework.stereotype.Component;
 @Component(value = "authRealm")
 public class AuthRealm extends AuthorizingRealm{
 
+	
+	/**  
+     * 认证  
+     */  
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+		return null;
+	}
+	
+	
+	 /**  
+     * 授权,只有成功通过doGetAuthenticationInfo方法的认证后才会执行。
+     */  
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
 		return null;
 	}
 
-	@Override
-	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-		return null;
-	}
+	
 
 }
